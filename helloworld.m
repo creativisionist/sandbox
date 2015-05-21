9f7dac17713a17bf663f6a1e45e3318f4be84c79
creativisionist <creativisionist@gmail.com>
2015-05-21 16:37:12 -0500

    Initial commit

---
+++ helloworld.m
@@ -0,0 +1,20 @@
+//
+//  helloworld.m
+//  
+//
+//  Created by Creativisionist on 5/21/15.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+    
+    @autoreleasepool {
+        
+        NSLog(@"Hello, World!");
+    
+    }
+    return 0;
+}
